1f32f0003f4edcdfe1404c1f7b3ebbd2f4892c94
pannawatth <92776344+pannawatth@users.noreply.github.com>
2022-09-12 14:15:00 +0700

    Update RTCCameraVideoCapturer.m

--- sdk/objc/components/capturer/RTCCameraVideoCapturer.m
+++ sdk/objc/components/capturer/RTCCameraVideoCapturer.m
@@ -143,6 +143,7 @@ const int64_t kNanosecondsPerSecond = 1000000000;
 - (void)startCaptureWithDevice:(AVCaptureDevice *)device
                         format:(AVCaptureDeviceFormat *)format
                            fps:(NSInteger)fps {
+  NSLog(@"start capturer is called from inside webrtc sdk !!!!");
   [self startCaptureWithDevice:device format:format fps:fps completionHandler:nil];
 }
 
