77339535f8ede8161d9e9fb9dac3cab2714b7b1a
pannawatth <pannawat@loolootech.com>
2022-09-15 13:35:34 +0700

    comment

--- sdk/objc/helpers/RTCCameraPreviewView.h
+++ sdk/objc/helpers/RTCCameraPreviewView.h
@@ -7,7 +7,7 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
-
+//Tanictest
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
